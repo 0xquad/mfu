9520798c78a98dbcb03e9d68be9453389876998b
Alexandre Hamelin <alexandre.hamelin@gmail.com>
2015-08-03 23:15:57 -0400

    Spacing

--- tests.py
+++ tests.py
@@ -40,6 +40,7 @@ class MFUTests(unittest.TestCase):
         content = bytearray.fromhex(content)
         self.card = MFUCard(bytes=content)
 
+
     def test_iter_bytes(self):
         iterator = iter(self.card)
         firstbytes = [next(iterator) for i in range(4)]
@@ -49,8 +50,10 @@ class MFUTests(unittest.TestCase):
         with self.assertRaises(StopIteration):
             next(iterator)
 
+
     def test_length(self):
         self.assertEqual(len(self.card), 64)
+
 
     def test_hexdump(self):
         output = []
@@ -83,6 +86,7 @@ class MFUTests(unittest.TestCase):
         )
         self.assertEqual(''.join(output), expected)
 
+
     def test_hexdump_with_custom_output(self):
         output = []
         def write(data):
@@ -110,6 +114,7 @@ class MFUTests(unittest.TestCase):
             '00000000\n'
         )
         self.assertEqual(''.join(output), expected)
+
 
     def test_dump(self):
         output = []
@@ -139,10 +144,12 @@ class MFUTests(unittest.TestCase):
         )
         self.assertEqual(b''.join(output), expected)
 
+
     def test_page_view_is_singleton(self):
         view1 = self.card.pages
         view2 = self.card.pages
         self.assertIs(view1, view2)
+
 
     def test_get_byte_by_index(self):
         self.assertEqual(self.card[0], 4)
@@ -153,6 +160,7 @@ class MFUTests(unittest.TestCase):
         self.assertEqual(self.card[-1], 0)
         self.assertEqual(self.card[-len(self.card)], 4)
 
+
     def test_get_bytes_by_slice(self):
         data = self.card[:4]
         self.assertEqual(data, b'\x04\xad\x71\x50')
@@ -160,6 +168,7 @@ class MFUTests(unittest.TestCase):
         self.assertEqual(data, b'\xe0\x00')
         data = self.card[60:]
         self.assertEqual(data, b'\x00\x00\x00\x00')
+
 
     def test_get_bytes_by_invalid_index(self):
         for i in (str, dict, list, tuple, set, bytes, bytearray,
@@ -171,14 +180,18 @@ class MFUTests(unittest.TestCase):
         with self.assertRaises(IndexError):
             self.card[len(self.card)]
 
+
     def test_get_page(self):
         self.assertEqual(bytes(self.card.pages[0]), b'\x04\xAD\x71\x50')
+
 
     def test_byte_by_getitem(self):
         self.assertEqual(self.card[0], 0x04)
 
+
     def test_bytes_by_slice(self):
         self.assertEqual(self.card[:2], b'\x04\xAD')
+
 
     def test_basic_attributes(self):
         self.assertEqual(self.card.id, 0x04ad71fada2e80)
@@ -193,9 +206,11 @@ class MFUTests(unittest.TestCase):
         self.assertEqual(self.card.id5, 0x2e)
         self.assertEqual(self.card.id6, 0x80)
 
+
     def test_first_pages_are_readonly(self):
         for p in [0, 1]:
             self.assertTrue(self.card.pages[p].readonly)
+
 
     def test_locked_pages_are_read_only(self):
         self.skipTest('not implemented')
@@ -204,10 +219,12 @@ class MFUTests(unittest.TestCase):
         #self.assertFalse(card.pages10to15_blocked)
         #self.assertFalse(card.otp_blocked)
 
+
     def test_init_default_empty(self):
         # initialized to all zeroes
         card = MFUCard()
         self.assertEqual(bytes(card), b'\x00' * 64)
+
 
     def test_init_one_param_only(self):
         with self.assertRaises(RuntimeError):
@@ -219,16 +236,19 @@ class MFUTests(unittest.TestCase):
         with self.assertRaises(RuntimeError):
             mfu = MFUCard(bytes=b'abcd'*4, file=1, hexfile=1)
 
+
     def test_init_bytestring(self):
         # initialized with bytes, must be 64 bytes
         card = MFUCard(bytes=b'\x01' * 64)
         self.assertEqual(bytes(card), b'\x01' * 64)
+
 
     def test_init_bytes(self):
         card = MFUCard(bytes=bytes(64))
         self.assertEqual(bytes(card), b'\x00' * 64)
         card = MFUCard(bytes=bytearray([i for i in range(64)]))
         self.assertEqual(list(card), [i for i in range(64)])
+
 
     def test_init_from_file(self):
         # load from a 64-byte binary file
@@ -240,6 +260,7 @@ class MFUTests(unittest.TestCase):
             card = MFUCard(file='card.bin')
             self.assertEqual(bytes(card), content)
 
+
     def test_init_from_file_descriptor(self):
         def sysread(desc, n):
             return b'\x01' * n
@@ -248,6 +269,7 @@ class MFUTests(unittest.TestCase):
             card = MFUCard(file=3)
             mock_sysread.assert_called_with(3, ANY)
             self.assertEqual(bytes(card), b'\x01' * 64)
+
 
     def test_init_from_hexfile(self):
         # load from an ASCII hex file, spaces ignored, case-insensitive
@@ -258,6 +280,7 @@ class MFUTests(unittest.TestCase):
              patch('os.read', return_value=content) as mock_sysread:
             card = MFUCard(hexfile='card.txt')
             self.assertEqual(bytes(card), b'\x0b\xad\xc0\xde' * 16)
+
 
     def test_init_from_hexfile_file_descriptor(self):
         def sysread(desc, n):
@@ -294,11 +317,14 @@ class MFUTests(unittest.TestCase):
                                 for i in range(16))
             self.assertEqual(bytes(card), expected)
 
+
+
 class MFUPageTests(unittest.TestCase):
     def __init__(self, name):
         super().__init__(name)
         card = MFUCard(bytes=bytes([1,2,3,4]*16))
         self.page = MFUPage(card, 0)
+
 
     def test_iter_bytes(self):
         byteiter = iter(self.page)
@@ -313,14 +339,17 @@ class MFUPageTests(unittest.TestCase):
         with self.assertRaises(StopIteration):
             next(byteiter)
 
+
     def test_as_list(self):
         bytelist = list(self.page)
         self.assertIsInstance(bytelist, list)
         self.assertEqual(bytelist, [1, 2, 3, 4])
 
+
     def test_slice(self):
         self.assertEqual(self.page[0], 1)
         self.assertEqual(self.page[1:-1], b'\x02\x03')
+
 
     @unittest.skip('item assignment is not implemented')
     def test_set_bytes_types(self):
@@ -331,11 +360,13 @@ class MFUPageTests(unittest.TestCase):
         self.page[0] = b'\x99'
         self.assertEqual(self.page[0], 0x99)
 
+
     @unittest.skip('item assignment is not implemented')
     def test_set_bytes_negative_index(self):
         self.assertNotEqual(self.page[-1], 99)
         self.page[-1] = 99
         self.assertEqual(self.page[-1], 99)
+
 
     @unittest.skip('item assignment is not implemented')
     def test_set_bytes_slice_value_types(self):
@@ -362,6 +393,7 @@ class MFUPageTests(unittest.TestCase):
         self.page[:2] = C()
         self.assertEqual(self.page[:2], b'\x13\x23')
 
+
     @unittest.skip('item assignment is not implemented')
     def test_set_bytes_invalid_value(self):
         for t in (str, complex, float, set, list, tuple, dict):
@@ -373,6 +405,7 @@ class MFUPageTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.page[0] = -1
 
+
     @unittest.skip('item assignment is not implemented')
     def test_set_bytes_invalid_index(self):
         for t in (str, complex, float, set, list, tuple, dict):
@@ -382,21 +415,26 @@ class MFUPageTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.page[5] = 0
 
+
     def test_invalid_index(self):
         for t in (str, list, set, dict, complex, object):
             with self.assertRaises(TypeError):
                 self.page[t()]
 
+
     def test_to_hex(self):
         hexstr = self.page.to_hex()
         self.assertEqual(hexstr, '01020304')
+
 
     def test_to_int(self):
         value = self.page.to_int()
         self.assertEqual(value, 0x01020304)
 
+
     def test_length(self):
         self.assertEqual(len(self.page), 4)
+
 
     def test_init_invalid_page(self):
         card = MFUCard()
@@ -405,10 +443,12 @@ class MFUPageTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             MFUPage(card, 16)
 
+
     def test_init_invalid_card(self):
         card = object()
         with self.assertRaises(TypeError):
             MFUPage(card, 0)
+
 
     def test_readonly(self):
         card = MFUCard()
@@ -441,20 +481,26 @@ class MFUPageTests(unittest.TestCase):
             # pages not locked
             self.assertFalse(pages[p].readonly)
 
+
+
 class MFUPageViewProxyTests(unittest.TestCase):
     def __init__(self, name):
         super().__init__(name)
         self.card = MFUCard()
 
+
     def test_length(self):
         self.assertEqual(len(self.card.pages), 16)
+
 
     def test_pages_proxy(self):
         self.assertIsInstance(self.card.pages, MFUPageViewProxy)
 
+
     def test_page_by_index(self):
         self.assertIsInstance(self.card.pages[0], MFUPage)
         self.assertIs(self.card.pages[-1], self.card.pages[15])
+
 
     def test_pages_by_slice(self):
         pages = self.card.pages[:2]
@@ -482,6 +528,7 @@ class MFUPageViewProxyTests(unittest.TestCase):
         self.assertEqual(len(pages), 1)
         self.assertTrue(all(isinstance(p, MFUPage) for p in pages))
 
+
     def test_page_by_invalid_index(self):
         with self.assertRaises(IndexError):
             self.card.pages[16]
@@ -489,6 +536,7 @@ class MFUPageViewProxyTests(unittest.TestCase):
         for t in (object, str, float, complex, bytes, bytearray):
             with self.assertRaises(TypeError):
                 self.card.pages[t()]
+
 
     def test_page_iterator(self):
         iterable = iter(self.card.pages)
@@ -500,10 +548,12 @@ class MFUPageViewProxyTests(unittest.TestCase):
         for i, p in enumerate(items):
             self.assertIs(p, self.card.pages[i + 1])
 
+
     def test_set_page_from_int(self):
         self.card.pages[0] = 0x11223344
         self.assertEqual(self.card.pages[0].to_int(), 0x11223344)
         self.assertEqual(self.card.pages[0].to_hex(), '11223344')
+
 
     def test_set_page_from_bytes(self):
         self.card.pages[0] = bytes([0x11, 0x22, 0x33, 0x44])
@@ -513,15 +563,18 @@ class MFUPageViewProxyTests(unittest.TestCase):
         self.assertEqual(self.card.pages[0].to_int(), 0x55667788)
         self.assertEqual(self.card.pages[0].to_hex(), '55667788')
 
+
     def test_set_page_from_bytearray(self):
         self.card.pages[0] = bytearray([0x11, 0x22, 0x33, 0x44])
         self.assertEqual(self.card.pages[0].to_int(), 0x11223344)
         self.assertEqual(self.card.pages[0].to_hex(), '11223344')
 
+
     def test_set_page_from_string(self):
         self.card.pages[0] = '\x11\x22\x33\x44'
         self.assertEqual(self.card.pages[0].to_int(), 0x11223344)
         self.assertEqual(self.card.pages[0].to_hex(), '11223344')
+
 
     def test_set_page_with_invalid_value(self):
         for t in (object, complex, float, dict, set, list, tuple):
@@ -530,14 +583,17 @@ class MFUPageViewProxyTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.card.pages[0] = None
 
+
     def test_set_page_with_invalid_int_index(self):
         with self.assertRaises(IndexError):
             self.card.pages[len(self.card.pages)] = 0
+
 
     def test_set_page_with_invalid_index(self):
         for t in (str, object, complex, float, dict, set, list, tuple):
             with self.assertRaises(TypeError):
                 self.card.pages[t()] = 0
+
 
     def test_set_page_slices_unsupported(self):
         with self.assertRaises(NotImplementedError):
