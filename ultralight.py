7876a205477a7991464c9e1a81e2f2a1a3a0b056
Alexandre Hamelin <alexandre.hamelin@gmail.com>
2015-08-03 23:04:22 -0400

    Added header

--- ultralight.py
+++ ultralight.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+#
+# A Python implementation for Mifare Ultralight cards.
+#
+# Copyright (c) 2015, Alexandre Hamelin <alexandre.hamelin gmail.com>
+#
+# This work is distributed under the LGPL license. See LICENSE.txt for details.
 
 
 import sys
